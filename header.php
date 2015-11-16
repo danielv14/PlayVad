92f84b787f5765db9547c8cbaf5ae9283f5fb05f
Daniel V <daniel.vernberg@gmail.com>
2015-11-16 14:13:59 +0100

    header på plats

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PlayVad</title>
+    <link rel="icon" href="favicon.ico">
+	   <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+<body>
