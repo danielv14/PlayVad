b5a089f4d530725cd83fc5ae744df724bf1dadab
Daniel V <daniel.vernberg@gmail.com>
2015-11-16 14:29:13 +0100

    Ändrade title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>PlayVad</title>
+    <title>PlayVad - Vi reder ut streamingdjungeln</title>
     <link rel="icon" href="favicon.ico">
 	   <link rel="stylesheet" href="css/style.css">
 
