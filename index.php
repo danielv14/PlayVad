3e5a6d14b83fc92611fb7183caba199a6cf4821a
Daniel V <daniel.vernberg@gmail.com>
2015-11-16 15:10:14 +0100

    lade till wrapper i index

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!-- Include header -->
 <?php include 'header.php';?>
 
-<h1>Hello world</h1>
 
+<!-- Main wrapper -->
+<div class="wrapper">
+  <h1>Hello world</h1>
+
+</div>
 
 <!-- Include footer -->
 <?php include 'footer.php';?>
