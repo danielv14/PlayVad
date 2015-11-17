d9863698af3c6de5a7c9bf2c9e054c4558ba0ddb
Daniel V <daniel.vernberg@gmail.com>
2015-11-17 16:37:25 +0100

    Skapade ett exempel på en till fullscreen img div längst ned. Vet ännu inte vad den ska innebära

--- index.php
+++ index.php
@@ -12,27 +12,29 @@
     <aside class="left">
       <img id="questionmark" src="img/questionmark2.png" alt="questionmark"/>
     </aside>
-    <h1>För mycket att välja bland...</h1>
-    <p>Visst finns det på tok för mycket streamingtjänster att välja bland? Det tycker vi också och
+    <h1><span class="marketing-title">Visst är streamingdjungeln svår?</span></h1>
+    <p><span class="marketing">Visst finns det på tok för mycket streamingtjänster att välja bland? Det tycker vi också och
     det är därför denna hemsida finns.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-    ex ea commodo consequat</p>
-    <br>
+    ex ea commodo consequat</span></p>
+  </article>
+
+  <article>
     <aside class="right">
       <img id="questionmark" src="img/answer.png" alt="questionmark"/>
     </aside>
-    <h1>Vi reder ut din frågor</h1>
-    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+    <h1><span class="marketing-title">Vi reder ut dina frågor!</span></h1>
+    <p><span class="marketing">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
   </article>
 </div><!-- end of nonfullscreen-div -->
 
 <!-- 4 mini columns here -->
-<div class="wrapper fullscreen-div space-below">
+<div class="wrapper fullscreen-div space-below space-above">
   <!-- mini parent containing its children -->
-  <div class="mini-parent">
+  <div class="mini-parent space-below">
     <!-- child element -->
     <div class="mini-element">
       <article>
@@ -74,6 +76,12 @@
       </article>
     </div>
   </div>
+</div>
+
+<!-- Image full width -->
+<div class="fullwidth-startpage2">
+  <h1>Läs om streamingtjänsterna</h1>
+  <a class="button-big" href="#">Visa mer</a>
 </div>
 
 
