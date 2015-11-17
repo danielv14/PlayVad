c02c0938d7efaae92b7340d3e33e1f98dc508502
Daniel V <daniel.vernberg@gmail.com>
2015-11-17 14:40:02 +0100

    html kod för fullscreen block som innehåller 4 mindre element med info om varje individuell tjänst och en call to action knapp

--- index.php
+++ index.php
@@ -17,8 +17,47 @@
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </article>
+</div><!-- end of nonfullscreen-div -->
 
-
+<div class="wrapper fullscreen-div">
+  <div class="mini-parent">
+    <div class="mini-element">
+      <article>
+        <h1>Netflix</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+        <a class="button" href="#">Visa mer</a>
+      </article>
+    </div>
+    <div class="mini-element">
+      <article>
+        <h1>Viaplay</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+        <a class="button" href="#">Visa mer</a>
+      </article>
+    </div>
+    <div class="mini-element">
+      <article>
+        <h1>HBO Nordic</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+        <a class="button" href="#">Visa mer</a>
+      </article>
+    </div>
+    <div class="mini-element">
+      <article>
+        <h1>CMore</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+        <a class="button" href="#">Visa mer</a>
+      </article>
+    </div>
+  </div>
 </div>
 
 <!-- Include footer -->
