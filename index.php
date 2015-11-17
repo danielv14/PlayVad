0294f1f392c09a21ee55f0951c0ff51ac71ea3c9
Daniel V <daniel.vernberg@gmail.com>
2015-11-17 22:06:24 +0100

    bold länk

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 <div class="fullwidth-startpage-streaming">
   <h1><span class="special-font">Streaming</span>tjänster?</h1>
   <h2 class="text-white special-font"><i>Vad innebär det och vilka är de bästa?</i></h2>
-  <a class="button-big" href="#">Läs mer</a>
+  <a class="button-big" href="#"><b>Läs mer</b></a>
 </div>
 
 
