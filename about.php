7ec8d1450f6b1bc9be5982f729be524feddcd5da
Daniel V <daniel.vernberg@gmail.com>
2015-11-18 10:29:44 +0100

    Skapade lite placeholder innehåll för att se hur about-sidan ska se ut

--- about.php
+++ about.php
@@ -1,9 +1,31 @@
 <!-- Include header -->
 <?php include 'header.php';?>
 
-<div class="wrapper nonfullscreen-div">
-  <h1>About</h1>
+<!-- Image full width -->
+<div class="fullwidth-about">
+  <h1>Om Play<span class="special-font marketing-title">Vad</span></h1>
+  <h2 class="text-white special-font"><i>Varför och av vem?</i></h2>
 </div>
+
+<!-- Wrapper -->
+<div class="wrapper nonfullscreen-div space-above space-below">
+  <article>
+    <h1 class="special-font header-bigger">Varför PlayVad</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <h1 class="special-font header-bigger space-above">Bakom PlaVad</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </article>
+</div> <!-- end of wrapper -->
 
 
 <!-- Include footer -->
