c8129ac6625b46afa69e2df13016c77fc49a0ffd
Daniel V <daniel.vernberg@gmail.com>
2015-11-18 14:58:06 +0100

    Sidan skapad. Lorem text används dock och är krav att ändras

--- streaming.php
+++ streaming.php
@@ -5,23 +5,59 @@
 <div class="fullwidth-streaming">
   <h1><span class="special-font marketing-title">Streaming</span>tjänster</h1>
   <h2 class="text-white special-font"><i>Vi listar de bästa av de bästa</i></h2>
-
 </div>
 
 <!-- Main wrapper -->
-<div class="wrapper nonfullscreen-div space-below space-above">
+<div class="wrapper nonfullscreen-div space-above">
   <article>
-    <h1 class="special-font header-bigger">Några ord innan vi börjar...</h1>
-
+    <h1 class="special-font header-bigger">Några meningar innan vi börjar...</h1>
     <p>Innan du börjar läsa på om vilka streamingtjänster vi rekommenderar måste vi först reda ut sen sak och
     det är just vad PlayVad klassar som streamingtjänst.
     Det PlayVad har valt att klassa som streamingtjänst är de tjänster som mot en prenumerationskostnad låter prenumeranterna strömma fritt från deras sortiment hur mycket det vill, när de vill. Streamingtjänster som fungerar som en digital hyrbutik är inget PlayVad tar med i ekvationen då vi inte anser att denna modell är lika bra. Hyr-modellen skulle dock vara bättre för individer som inte konsumerar så mycket media.</p>
-
-    <p>Låt säga att en person ser på 2 filmer i månaden på ett ungefär. Om denne person skulle prenumerera på en stremaingtjänst för 80kr/månaden skulle varje film ha kostad personen 40kr styck. Om samma individ skulle använda en digital hyr-butik där de två filmerna kostar 20kr/styck skulle personen bara ha lagt 40kr den månaden på media. Hur mycket media man konsumerar månadsvis avgör vilken slags streamingtjänst som passar bäst. Förslag på digitala hyr-butiker är <a class="link" href="https://www.plejmo.com/sv/">Plejmo</a> och <a class="link" href="http://sfanytime.com/sv-SE/">SF Anytime</a>.</p>
-
+    <p>Låt säga att en person ser på 2 filmer i månaden på ett ungefär. Om denne person skulle prenumerera på en stremaingtjänst för 80kr/månaden skulle varje film ha kostad personen 40kr styck. Om samma individ skulle använda en digital hyr-butik där de två filmerna kostar 20kr/styck skulle personen bara ha lagt 40kr den månaden på media. Hur mycket media man konsumerar månadsvis avgör vilken slags streamingtjänst som passar bäst. Förslag på digitala hyr-butiker är <a class="link" href="https://www.plejmo.com/sv/">Plejmo</a> och <a class="link" href="http://sfanytime.com/sv-SE/">SF Anytime</a> men PlayVad går inte in mer i detalj om dessa därför att digitala hyr-butiker ingår inte i PlayVads klassifikation av streamingtjänster.</p>
   </article>
-
 </div>
+
+<!-- div wrapper fullscreen -->
+<div class="wrapper fullscreen-div space-below">
+  <div class="mini-parent">
+    <div class="mini-element-2col">
+      <article>
+        <h1 class="special-font header-big space-above">Netflix</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a class="button" href="#">Mer om Netflix</a>
+      </article>
+    </div> <!-- End of mini element-->
+    <div class="mini-element-2col">
+      <article>
+        <h1 class="special-font header-big space-above">HBO Nordic</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a class="button" href="#">Mer om HBO Nordic</a>
+      </article>
+    </div><!-- End of mini-element -->
+  </div><!-- End of parent -->
+  <div class="mini-parent">
+    <div class="mini-element-2col">
+      <article>
+        <h1 class="special-font header-big space-above">C More</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a class="button" href="#">Mer om C More</a>
+      </article>
+    </div><!-- End of mini element -->
+    <div class="mini-element-2col">
+      <article>
+        <h1 class="special-font header-big space-above">Viaplay</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a class="button" href="#">Mer om Viaplay</a>
+      </article>
+    </div><!-- End of mini element -->
+  </div><!-- End of parent-->
+</div><!-- End of wrapper -->
+
+
+
+
+
 
 
 <!-- Include footer -->
