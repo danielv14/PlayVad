9dfeae7fff810ab4fef6224cf556b6877f6b6a69
Daniel V <daniel.vernberg@gmail.com>
2015-11-18 11:53:39 +0100

    aboutsidan skapad

--- about.php
+++ about.php
@@ -4,26 +4,29 @@
 <!-- Image full width -->
 <div class="fullwidth-about">
   <h1>Om Play<span class="special-font marketing-title">Vad</span></h1>
-  <h2 class="text-white special-font"><i>Varför och av vem?</i></h2>
+  <h2 class="text-white special-font"><i>Varför och hur?</i></h2>
 </div>
 
 <!-- Wrapper -->
 <div class="wrapper nonfullscreen-div space-above space-below">
   <article>
     <h1 class="special-font header-bigger">Varför PlayVad</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <h1 class="special-font header-bigger space-above">Bakom PlaVad</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <p>Idag finns det en uppsjö olika sätt att konsumera media på. Detta tycker vi såklart är väldigt bra. Men som med allt annat kan det bli svårt när för mycket valmöjligheter finns till vårt förfogande. Snabbare internet och bättre internettjänster har gjort att ett nytt sätt att konsumera film och tv-serier har uppstått. Människor kallar sig <a class="link" href="https://en.wikipedia.org/wiki/Cord-cutting">Cord-cutters</a> när de lämnar traditionellt tv-tittande bakom sig och vänder sig till internet och streamingtjänster.</p>
+    <p>Som resultat av detta har det uppstått många så kallade streamingtjänster där kunderna själva bestämmer när, var och hur exempelvis senaste säsongen av Breaking Bad ska konsumeras. Med hjälp av streamingtjänster tar konsumenten kontroll över tv-tablån och behöver inte anpassa sig på samma sätt längre som med mer traditionellt tv-tittande. Inte längre behöver man damma av stegen, gå upp på taket och justera parabolen för att mottagningen är för dålig. Det enda som behövs är en stabil internetuppkoppling, som de flesta hushåll redan har idag, samt en prenumeration på en streamingtjänst. Men då det finns så många streamingtjänster kan det vara svårt att veta vilken som skulle passa en bäst&hellip;</p>
+    <p>PlayVad finns för att råda bot på just detta problem! PlayVad finns här för att ge dig mer kunskap om vilka de ledande streamingtjänsterna är, vad de innebär och vad du kan förvänta dig av en prenumeration hos dessa. PlayVad reder ut streamingdjungeln, helt enkelt!</p>
+
+    <h1 class="special-font header-bigger space-above">Vad PlayVad gör</h1>
+    <p>Hur reder man då ut denna streamingdjungel kanske du undrar? Många timmars research och många liter té har gått åt till att samla in fakta om de ledande streamingtjänsterna idag. Sedan presenterar vi denna fakta för dig i hopp om att du själv ska kunna avgöra vilken eller vilka streamingtjänster som passar just <em>dig</em> bäst.</p>
+    <p>Den fakta vi har valt att presentera om streamingtjänsterna är: </p>
+    <ul>
+      <li>Kostnad</li>
+      <li>Utbud av film och tv-serier</li>
+      <li>Eventuella bindningstider</li>
+      <li>Barn- och sportanpassat innehåll</li>
+      <li>Smarta appar</li>
+      <li>Hur många kan streama från samma konto samtidigt</li>
+      <li>Individanpassning</li>
+    </ul>
   </article>
 </div> <!-- end of wrapper -->
 
