6553b6d0046ea2aef94a5b058f4d06edf1605041
Daniel V <daniel.vernberg@gmail.com>
2015-11-18 13:17:00 +0100

    korrekt indent

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <title>PlayVad - Vi reder ut streamingdjungeln</title>
     <link rel="icon" href="favicon.ico">
-	   <link rel="stylesheet" href="css/style.css">
-     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-     <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+	  <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
   </head>
 
 <body>
