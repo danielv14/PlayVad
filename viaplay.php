c20f940a59ff6ec1aa1591f478ae033a37aa5926
Daniel V <daniel.vernberg@gmail.com>
2015-11-19 14:41:13 +0100

    Använder en img-shadow samt lägger till en till aside på slutet som länkar till artiklar om andra streamingtjänster

--- viaplay.php
+++ viaplay.php
@@ -2,7 +2,7 @@
 <?php include 'header.php';?>
 
 <!-- Full width img netflix -->
-<div class="fullwidth-viaplay"></div>
+<div class="fullwidth-viaplay img-shadow"></div>
 
 <!-- main wrapper -->
 <div class="wrapper fullscreen-div space-above space-below">
@@ -55,13 +55,29 @@
 
 
 
+
+
+
   <div class="article-content space-above">
     <article>
       <h2 class="special-font header-big header-space-above-normal">Kik ka</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
 
+  <!-- aside element -->
+  <div class="article-aside right">
+    <aside>
+      <h2>Läs även om...</h2>
+      <h3><a class="link" href="hbonordic.php">HBO Nordic</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="netflix.php">Netflix</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="cmore.php">C More</a></h3>
+    </aside>
+  </div>
+
+  <div class="article-content">
+    <article>
       <h2 id="avslutningsvis" class="special-font header-big header-space-above-normal">Avslutningsvis</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
