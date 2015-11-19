f6e15cb4b6abeaaad796dab4964964477691277a
Daniel V <daniel.vernberg@gmail.com>
2015-11-19 13:09:34 +0100

    lägger till möjlighet för font-awesome

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
 	  <link rel="stylesheet" href="css/style.css">
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"><!-- font awesome -->
+
   </head>
 
 <body>
