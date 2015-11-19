fa2023194ea89042004f68706ddb62f412f7bf88
Daniel V <daniel.vernberg@gmail.com>
2015-11-19 13:25:52 +0100

    skapade en grundstruktur och upplägg för hur artiklarna ska se ut

--- netflix.php
+++ netflix.php
@@ -7,30 +7,56 @@
 <!-- main wrapper -->
 <div class="wrapper fullscreen-div space-above space-below">
 
-    <!-- aside element -->
-    <div class="article-aside right">
-      <aside>
-        <h2>Innehåll</h2>
-        <h3><a class="link" href="#">Bla Bla</a></h3>
-        <h3 class="header-space-above-small"><a class="link" href="#">Ta ha ta ha</a></h3>
-        <h3 class="header-space-above-small"><a class="link" href="#">Kik ka</a></h3>
-        <h3 class="header-space-above-small"><a class="link" href="#avslutningsvis">Avslutningsvis</a></h3>
-      </aside>
-    </div>
+  <!-- aside element -->
+  <div class="article-aside right">
+    <aside>
+      <h2>Innehåll</h2>
+      <h3><a class="link" href="#">Bla Bla</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#plattformar">Plattformar</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#">Kik ka</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#avslutningsvis">Avslutningsvis</a></h3>
+    </aside>
+  </div>
 
-    <div class="article-content">
+  <!-- article-content non-fullscreen -->
+  <div class="article-content">
+    <article>
       <h1 class="special-font header-biggest">Netflix</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <h2 class="special-font header-big header-space-above-normal">Bla Bla</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
+  </div><!-- end of article-content non-fullscreen -->
 
-      <h2 class="special-font header-big header-space-above-normal">Ta ha ta ha</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- article-content-fullscreen -->
+  <div id="plattformar" class="article-content-fullscreen">
+    <article>
+      <h2 class="special-font header-big header-space-above-normal header-center">Netflix finns på följade plattformar</h2>
+      <ul class="list-col">
+        <li><i class="fa fa-desktop"></i> PC</li>
+        <li><i class="fa fa-apple"></i> Mac</li>
+        <li><i class="fa fa-chrome"></i> Chrome OS</li>
+        <li><i class="fa fa-mobile"></i> iOS</li>
+        <li><i class="fa fa-mobile"></i> Android</li>
+        <li><i class="fa fa-mobile"></i> Windows Phone</li>
+        <li><i class="fa fa-chrome"></i> Chromecast</li>
+        <li><i class="fa fa-apple"></i> Apple TV</li>
+        <li><i class="fa fa-television"></i> Smart TV apparater </li>
+        <li><i class="fa fa-gamepad"></i> Playstation 3 &amp; 4</li>
+        <li><i class="fa fa-gamepad"></i> Wii &amp; Wii U</li>
+        <li><i class="fa fa-gamepad"></i> Xbox 360 &amp; Xbox One</li>
 
+
+      </ul>
+    </article>
+  </div><!-- End of article-content-fullscreen -->
+
+
+
+  <div class="article-content">
+    <article>
       <h2 class="special-font header-big header-space-above-normal">Kik ka</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -40,12 +66,10 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
+  </div>
 
-
-    </div>
-
-
-</div>
+</div><!-- end of parent-fullscreen wrapper -->
 
 
 <!-- Include footer -->
