efd19962409e20c94eec1599c12b32a4c2d4f17f
Daniel V <daniel.vernberg@gmail.com>
2015-11-19 13:56:14 +0100

    placeholder för C More

--- cmore.php
+++ cmore.php
@@ -1,9 +1,76 @@
 <!-- Include header -->
 <?php include 'header.php';?>
 
-<div class="wrapper nonfullscreen-div">
-  <h1>Cmore</h1>
-</div>
+<!-- Full width img netflix -->
+<div class="fullwidth-cmore"></div>
+
+<!-- main wrapper -->
+<div class="wrapper fullscreen-div space-above space-below">
+
+  <!-- aside element -->
+  <div class="article-aside right">
+    <aside>
+      <h2>Innehåll</h2>
+      <h3><a class="link" href="#">Bla Bla</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#plattformar">Plattformar</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#">Kik ka</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#avslutningsvis">Avslutningsvis</a></h3>
+    </aside>
+  </div>
+
+  <!-- article-content non-fullscreen -->
+  <div class="article-content space-below">
+    <article>
+      <h1 class="special-font header-biggest">C More</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2 class="special-font header-big header-space-above-normal">Bla Bla</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
+  </div><!-- end of article-content non-fullscreen -->
+
+  <!-- article-content-fullscreen -->
+  <div id="plattformar" class="article-content-fullscreen">
+    <article>
+      <h2 class="special-font header-big header-space-above-normal header-center">C More finns på följade plattformar</h2>
+      <ul class="list-col">
+        <li><i class="fa fa-desktop"></i> PC</li>
+        <li><i class="fa fa-apple"></i> Mac</li>
+        <li><i class="fa fa-chrome"></i> Chrome OS</li>
+        <li><i class="fa fa-mobile"></i> iOS</li>
+        <li><i class="fa fa-mobile"></i> Android</li>
+        <li><i class="fa fa-mobile"></i> Windows Phone</li>
+        <li><i class="fa fa-chrome"></i> Chromecast</li>
+        <li><i class="fa fa-apple"></i> Apple TV</li>
+        <li><i class="fa fa-television"></i> Smart TV apparater </li>
+        <li><i class="fa fa-gamepad"></i> Playstation 3 &amp; 4</li>
+        <li><i class="fa fa-gamepad"></i> Wii &amp; Wii U</li>
+        <li><i class="fa fa-gamepad"></i> Xbox 360 &amp; Xbox One</li>
+
+
+      </ul>
+    </article>
+  </div><!-- End of article-content-fullscreen -->
+
+
+
+  <div class="article-content space-above">
+    <article>
+      <h2 class="special-font header-big header-space-above-normal">Kik ka</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      <h2 id="avslutningsvis" class="special-font header-big header-space-above-normal">Avslutningsvis</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
+  </div>
+
+</div><!-- end of parent-fullscreen wrapper -->
+
 
 <!-- Include footer -->
 <?php include 'footer.php';?>
