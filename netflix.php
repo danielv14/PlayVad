89a926978079b21a813700befb830a0f98886e2d
Daniel V <daniel.vernberg@gmail.com>
2015-11-23 15:28:37 +0100

    Netflix sidan är på plats!

--- netflix.php
+++ netflix.php
@@ -11,22 +11,57 @@
   <div class="article-aside right">
     <aside>
       <h2 class="special-font">Innehåll</h2>
-      <h3><a class="link" href="#">Bla Bla</a></h3>
+      <h3><a class="link" href="#inledning">Inledning</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#utbud">Utbud</a></h3>
       <h3 class="header-space-above-small"><a class="link" href="#plattformar">Plattformar</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#">Kik ka</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#avslutningsvis">Avslutningsvis</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#kostnad">Kostnad och bindningstid</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#streamare">Aktiva streamare</a></h3>
+      <h3 class="header-space-above-small"><a class="link" href="#individanpassning">Individanpassning</a></h3>
     </aside>
   </div>
 
   <!-- article-content non-fullscreen -->
   <div class="article-content space-below">
     <article>
-      <h1 class="special-font header-biggest">Netflix</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <h2 class="special-font header-big header-space-above-normal">Bla Bla</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1 id="inledning" class="special-font header-bigger">Inledning</h1>
+      <p>Netflix är ett amerikanskt företag som startade redan 1997 och har lyckats anpassa sig för den digitala efterfrågan av streaming. År 2012 lanserades tjänsten i norden. Streamingutbudet som består av filmer, tv-serier och till och med egenproducerat material. Utbudet varierar beroende på vilket land man befinner sig i, men detta gäller dock inte det egenproducerade innehållet. Tjänsten är tillgänglig på en uppsjö olika plattformar och beroende på vilken typ av månadskostnad man betalar kan olika antal personer streama samtidigt</p>
+
+      <h1 id="utbud" class="special-font header-bigger space-above">Utbud</h1>
+      <h2 class="special-font">Film</h2>
+      <p>1000-tals filmer finns tillgängligt att streama från Netflix. Allt från krigsfilmer, komedier, skräck och indiefilmer finns att titta på.</p>
+      <p>När det finns så pass många filmer till ens förfogande kan det lätt bli överväldigande. Netflix försöker råda bot på detta genom att det går att kategorisera urvalet väl. Låt säga att du är sugen på att se en satir av något slag. Att leta igenom 1000-tals filmer för att hitta en satir är då inte ett speciellt effektivt sätt att hitta rätt film. De man då kan göra är att bläddra med hjälp av genrer och undergenrer för att sålla bort alla filmer som inte är satirer, i det här fallet. Netflix har alltså tagit fram verktyg för att en användare snabbt ska kunna hitta rätt.</p>
+      <h2 class="special-font">Tv-Serier</h2>
+      <p>Även utbudet för tv-serier är väldigt stort och samma filtrering och sökmöjligheter finns att använda sig av för att hitta intressanta tv-serier, precis som med filmer.</p>
+      <p>Något Netflix har börjat göra allt mer och mer är att göra egna tv-serier av väldigt hög kvalité som i många fall tas emot väldigt bra av sina användare och det skulle inte förvåna mig om många prenumeranter är just det för att Netflix egenproducerade innehåll är så pass bra.</p>
+      <h3 class="special-font">Binge-watching</h3>
+      <p>Detta är ett <a class="link" href="https://en.wikipedia.org/wiki/Binge-watching">fenomen</a> som inte nödvändigtvis är synonymt med just Netflix men är något som Netflix möjligör då många av de egenproducerade tv-seriernas säsonger släpps på en och samma dag. Traditionellt sätt, om man följer en tv-serie, kommer det ju ett avsnitt i veckan. Så är inte fallet med många av Netflix egna tv-serier. Många gånger släpps en hel säsong av en tv-serie samma dag och detta innebär att en prenumerant i princip kan sträcktitta på tv-serien från säsongens första avsnitt till det sista i ett svep. Det är just det som binge-watching betyder, tittandet på tv-serier blir ett maraton. Detta sätt att släppa tv-serier på är något som uppskattas av många och gör att användare själva blir i kontroll av i vilken takt de väljer att konsumera en tv-serie.</p>
+      <h3 class="special-font">Exempel på egenproducerade tv-serier</h3>
+      <ul>
+      <li>House of Cards</li>
+      <li>Orange Is The New black</li>
+      <li>Grace and Frankie</li>
+      <li>Sense8</li>
+      <li>Daredevil</li>
+      <li>Jessica Jones</li>
+      <li>Narcos</li>
+      <li>Master of None</li>
+      <li>BoJack Horseman</li>
+      </ul>
+      <h2 class="special-font">Barn</h2>
+      <p>Netflix är väldigt bra på att separera innehållet för barn från de vanliga innehållet. Det finns en separat <a class="link" href="#profiler">profil</a> som heter <em>Kids</em> där endast lämpligt innehåll för barn visas. Denna profil innehåller kategorier såsom allsång, Disney, talande djur, havsäventyr, utbildning och en kategori som lämpar sig för hela familjen.</p>
+      <p>Netflix gör alltså ett bra jobb på att anpassa innehållet och leverera innehåll som lämpar sig för barn.</p>
+      <h2 class="special-font">Sport</h2>
+      <p>För tillfället ingår inte sport i Netflix utbud mer en eventuella filmer, dokumentärer och tv-serier om sport. Vill man läsa mer om Netflix ståndpunkt i frågan kan man läsa om det <a href="http://www.cinemablend.com/television/Netflix-Ever-Broadcast-Live-Sports-Here-What-Company-Says-71968.html">här</a></p>
+      <h2 class="special-font">Dokumentärer</h2>
+      <p>Netflix erbjuder en hel del (mer än 240) dokumentärer av olika slag och precis som med utbudet av tv-seier erbjuder Netflix några egenproducerade dokumentärer.</p>
+      <p>Netflix tillhandahåller exempelvis politiska, biografiska, vetenskapliga och samhällsdokumentärer.</p>
+      <h3 class="special-font">Exempel på egenproducerade dokumentärer</h3>
+      <ul>
+      <li>Virunga</li>
+      <li>The Short Game</li>
+      <li>My Own Man</li>
+      <li>Winter On Fire</li>
+      </ul>
     </article>
   </div><!-- end of article-content non-fullscreen -->
 
@@ -54,10 +89,33 @@
 
   <div class="article-content space-above">
     <article>
-      <h2 class="special-font header-big header-space-above-normal">Kik ka</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1 id="kostnad" class="special-font header-bigger">Kostnad och bindningstid</h1>
+      <h2 class="special-font">Prova på</h2>
+      <p>Netflix erbjuder en &ldquo;prova på&rdquo; tjänst för alla nya potentiella kunder där man kan
+      kan prova på tjänsten i 1 månad. Under denna månad har man fritt tillträde till hela
+      utbudet av både film och serier. Om man bestämmer sig för att man vill fortsätta
+      vara prenumerant av Netflix behöver man inget göra efter att gratismånaden har upphört.
+      Om man däremot inte vill fortsätta efter sin första månad måste man aktivt avsluta
+      prenumerationen.</p>
+      <h2 class="special-font">Kostnad</h2>
+      <p>Netflix har 3 olika abonnemang som innebär lite olika saker. Det första abonnemanget kostar 79kr/månad och innebär att det går att titta på en skärm åt gången i standardupplösning. &ldquo;Mellanabonnemanget&rdquo; kostar 89kr/månad och med detta abonnemang kan man titta på 2 skärmar samtidigt i HD kvalité, om ens Internet och TV klarar av det. Det tredje och sista abonnemanget kostar 119kr/månad där det går att titta på upp till 4 skärmar samtidigt. Utöver HD kvalité har även detta abonnemang stöd för så kallad <a href="https://sv.wikipedia.org/wiki/Ultra_High_Definition_Television">Ultra HD</a>.</p>
+      <p>Alla abonnemangen erbjuder obegränsat med tv-serier och filmer. Det som skiljer abonnemangen åt är alltså hur många enheter som kan streama samtidigt samt den tillgängliga kvaliteten. Som potentiell prenumerant är det alltså viktigt att ta sig en funderade och se över ens egna behov eller sin familjs behov.</p>
+      <h2 class="special-font">Bindningstid</h2>
+      <p>Om man väljer att vara Netflix-prenumerant faktureras man månadsvis men helt utan bindningstid. En prenumerant kan när som helst välja att avsluta sin prenumeration.</p>
+
+      <h1 id="streamare" class="special-font header-bigger space-above">Aktiva Streamare</h1>
+      <p>Som redan beskrivet under rubriken <a class="link" href="#kostnad">Kostnad</a> avgör vilken typ av abonnemang man har hur många som kan streama innehåll från Netflix samtidigt. Kort sagt handlar det om att antingen 1, 2 eller 4 skärmar kan strömma innehåll från Netflix samtidigt.</p>
+
+      <h1 id="individanpassning" class="special-font header-bigger space-above">Individanpassning</h1>
+      <p>Netflix möjligör individanpassning på en rad olika smarta sätt, allt för att varje enskild användare snabbt ska komma åt innehåll som passar individen. Med hjälp av profiler, listor och anpassade förslag försöker Netflix anpassa upplevelsen på bästa möjliga sätt.</p>
+      <h2 id="profiler" class="special-font">Profiler</h2>
+      <p>Med profiler menar Netflix att olika medlemmar i exempelvis ett hushåll kan ha en egen, anpassad Netflix-upplevelse som baseras på filmerna och tv-serierna de gillar. Det går att skapa upp till 5 olika profiler där profilerna kan döpas om och få bilder, så kallade avatarer.  Profilerna är själva basen i hur Netflix individanpassar sitt innehåll där listor och anpassade förslag baserar sig på vilken profil som är aktiv.</p>
+      <h2 class="special-font">Min lista</h2>
+      <p>Då Netflix innehållskatalog är så pass stor är risken stor att man tappar bort sig i innehållet. Låt säga att du bläddrar i innehållet och letar efter grymma action-filmer. Du är ute efter en film med Sylvester Stallone men hittar en film med Arnold Schwarzenegger som verkar grym. Nu var du ju ute efter en film med Stallone men vet att du kommer vilja se den andra filmen någon annan gång. Du är dock rädd för att du inte kommer komma ihåg vad filmen med Schwarzenegger heter eller att du glömmer bort att den ens finns. Det du då kan göra är att lägga till Schwarzenegger-filmen i din lista. Schwarzenegger-filmen kommer då att finnas i din lista tills det att du väljer att ta bort den därifrån igen. Listan är alltså ett kraftfultl sätt att spara filmer och tv-serier som du är intresserade av.</p>
+      <p>Listan är anpassad efter profilerna och när exempelvis Schwarzenegger-filmen läggs till i listan för en profil läggs den inte till de andra profilernas listor.</p>
+      <h2 class="special-font">Anpassade förslag för filmer och tv-serier</h2>
+      <p>För att bygga vidare på exemplet om action-filmer med Sylvester Stallone kommer Netflix att rekommendera och ge förslag på liknande filmer efter att du har sätt filmen med Stallone. Låt säga att Stallone-filmen var <a class="link" href="http://www.imdb.com/title/tt0083944/?ref_=tt_rec_tt">Rambo: First Blood</a>. Det Netflix då gör är att ge dig förslag på liknande filmer och exempelvis skulle du via Netflix förslag bli tipsad om uppföljaren, <a class="link" href="http://www.imdb.com/title/tt0089880/?ref_=tt_rec_tt">Rambo: FIrst Blood II</a>. Detta då förutsatt att Netflix har bägge filmerna i sitt sortiment.</p>
+      <p>Tanken med anpassade förslag är att ju mer filmer och tv-serier man tittar på via en profil ju bättre blir Netflix på att förstå din smak och kan ge tips därefter.</p>
     </div>
 
     <!-- aside element -->
@@ -72,10 +130,11 @@
 
     <div class="article-content">
       <article>
-        <h2 id="avslutningsvis" class="special-font header-big header-space-above-normal">Avslutningsvis</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h2 class="special-font">Betyg</h2>
+        <p>Innehåll i Netflix utbud, vare sig det är filmer, tv-serier eller dokumentärer har en betygsskala på 5 stjärnor kopplat till sig. Netflix försöker att förutse vad du kommer att tycka om exempelvis en film och visar detta med hjälp av en betygsskala. Om en film exempelvis har 4.5 <em>röda</em> stjärnor tror Netflix att du skulle tycka om den filmen ganska mycket. &ldquo;Upp till bevis!&rdquo; tänker du och bestämmer dig för att se filmen. Om det visar sig att Netflix förväntade betyg stämde kan du själv sätta betyg på filmen och förslagsvis ge den 5 av 5 stjärnor. Det som händer då är att filmens stjärnor ändrar färg och blir <em>gula</em> istället. Gula stjärnor visar alltså betyg som du själv har satt.</p>
+        <p>Beroende på hur &ldquo;duktig&rdquo; du är på att betygsätta titlar ju bättre blir Netflix på att förutse vad du skulle tycka om andra titlar.</p>
+        <h2 class="special-font">Ta individanpassningen med en nypa salt&hellip;</h2>
+        <p>Värt att påpeka är att Netflix individanpassning bör tas med en nya salt. Människor är komplexa varelser och ens smak när det gäller film och tv-serier är inte alltid så svart och vit. Många människor gillar filmer och tv-serier som sträcker sig över en rad olika genrer och detta gör Netflix jobb att individanpassa något svårt. Netflix känner inte dig bättre än vad du själv känner dig, något annat vore lite läskigt. individanpassningen är inget man ska stirra sig blint på och även om Netflix <em>tror</em> att du inte skulle gilla en film behöver det nödvändigtvis inte vara sant. Ett tips om individanpassningen är att främst uutgå från den egna listan, som användaren själv faktiskt har 100% kontroll över.</p>
       </article>
     </div>
 
