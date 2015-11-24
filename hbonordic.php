9b3e38dcb75f14832a83442706e499c1b8159e3f
Daniel V <daniel.vernberg@gmail.com>
2015-11-24 10:01:26 +0100

    htmlkod för hbo nordic är på plats

--- hbonordic.php
+++ hbonordic.php
@@ -11,22 +11,46 @@
   <div class="article-aside right">
     <aside>
       <h2 class="special-font">Innehåll</h2>
-      <h3><a class="link" href="#">Bla Bla</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#plattformar">Plattformar</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#">Kik ka</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#avslutningsvis">Avslutningsvis</a></h3>
+      <h3><a class="link" href="#inledning">Inledning</a></h3>
+      <h3><a class="link" href="#utbud">Utbud</a></h3>
+      <h3><a class="link" href="#plattformar">Plattformar</a></h3>
+      <h3><a class="link" href="#kostnad">Kostnad och bindningstid</a></h3>
+      <h3><a class="link" href="#streamare">Aktiva streamare</a></h3>
+      <h3><a class="link" href="#individanpassning">Individanpassning</a></h3>
     </aside>
   </div>
 
   <!-- article-content non-fullscreen -->
   <div class="article-content space-below">
     <article>
-      <h1 class="special-font header-biggest">HBO Nordic</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <h2 class="special-font header-big header-space-above-normal">Bla Bla</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1 id="inledning"class="special-font header-bigger">Inledning</h1>
+      <p>HBO Nordic är det amerikanska TV-bolaget <a class="link" href="http://www.hbo.com">HBOs</a> sätt att nå ut till den nordiska marknaden. Tjänsten landerades vintern 2012 och sedan dess har det gått att streama HBO's egenproducerade tv-serier och tv-serier från andra producenter. Även om tjänsten erbjuder filmer är de flesta prenumeranter intresserade av HBO's egna tv-serier som ofta håller väldigt hög kvalité och där många anses som "klassiker". Tjänsten finns tillgänglig på många olika plattformar. Två personer kan streama samtidigt på ett och samma konto.</p>
+
+      <h1 id="utbud"class="special-font header-bigger space-above">Utbud</h1>
+      <h2 class="special-font">Film</h2>
+      <p>I HBO Nordics utbud hittar man hundratals &ldquo;handplockade&rdquo; filmer inom genres som komedi, romantik, thriller och western. Även om utbudet av filmer inte är speciellt stort, jämfört med tv-serieutbudet finns det förmodligen något för alla inom filmutbudet.</p>
+      <h2 class="special-font">Tv-Serier</h2>
+      <p>Utan att överdiva kan man säga att tv-serier är HBO Nordics största styrka och flaggskepp. Med över 3500 serieavsnitt från de allra mest hyllade tv-serierna levererar HBO Nordic tv-serier i hög klass. Tv-serier som handlar om pirater, krig och hobby-detektiver får alla plats i HBO Nordics utbud.</p>
+      <p>Utbudet av tv-serier i sig är värt nog att bli prenumerant för, om man vet med sig att man gillar tv-serier från HBO.</p>
+      <h3 class="special-font">Originalserier från HBO</h3>
+      <p>HBO Nordics största styrka är att erbjuda deras egenproducerade tv-serier som ofta är välkända och kritikerrosade. Här nedan följer ett axplock av dessa originalserier som går att streama från HBO Nordic:</p>
+      <ul>
+      <li>Game of Thrones</li>
+      <li>True Blood</li>
+      <li>Oz</li>
+      <li>Deadwood</li>
+      <li>The Wire</li>
+      <li>Six Feet Under</li>
+      <li>The Sopranos</li>
+      <li>Sex and the City</li>
+      <li>True Detective</li>
+      </ul>
+      <h2 class="special-font">Barn</h2>
+      <p>Barnutbudet hos HBO Nordic är väldigt dåligt och snudd på obefintligt. HBO Nordic har riktat in sig mot en mer äldre målgrupp och som detta lyser utbud för barn med sin frånvaro.</p>
+      <h2 class="special-font">Sport</h2>
+      <p>HBO Nordic erbjuder för tillfället inga sportsändningar utan fokuserar sitt fokus till 100% på filmer och tv-serier av hög kvalité.</p>
+      <h2 class="special-font">Dokumentärer</h2>
+      <p>Över 170 dokumentärer ingår i utbudet från HBO Nordic. Dokumentärerna handlar om spridda ämnen, om allt från medicin till elefanter och kriminalporträtt.</p>
     </article>
   </div><!-- end of article-content non-fullscreen -->
 
@@ -37,16 +61,14 @@
       <ul class="list-col">
         <li><i class="fa fa-desktop"></i> PC</li>
         <li><i class="fa fa-apple"></i> Mac</li>
-        <li><i class="fa fa-chrome"></i> Chrome OS</li>
         <li><i class="fa fa-mobile"></i> iOS</li>
         <li><i class="fa fa-mobile"></i> Android</li>
         <li><i class="fa fa-mobile"></i> Windows Phone</li>
         <li><i class="fa fa-chrome"></i> Chromecast</li>
         <li><i class="fa fa-apple"></i> Apple TV</li>
-        <li><i class="fa fa-television"></i> Smart TV apparater </li>
+        <li><i class="fa fa-television"></i> Samsung Smart Tv (2012-2015) </li>
         <li><i class="fa fa-gamepad"></i> Playstation 3 &amp; 4</li>
-        <li><i class="fa fa-gamepad"></i> Wii &amp; Wii U</li>
-        <li><i class="fa fa-gamepad"></i> Xbox 360 &amp; Xbox One</li>
+        <li><i class="fa fa-gamepad"></i> Xbox 360</li>
 
 
       </ul>
@@ -57,12 +79,19 @@
 
   <div class="article-content space-above">
     <article>
-      <h2 class="special-font header-big header-space-above-normal">Kik ka</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
+      <h1 id="kostnad"class="special-font header-bigger">Kostnad och bindningstid</h1>
+      <h2 class="special-font">Prova på</h2>
+      <p>HBO Nordic tillåter potentiella nya kunder en prova-på period på en månad, kostnadsfritt. Under denna prova-på månad går det att nyttja hela HBO Nordics utbud till 100%. Om man skulle vilja fortsätta prenumerationen efter prova-på månaden behövs inget göras. Abonnemanget övergår till ett vanligt abonnemang efter att det har gått en månad. Om man däremot inte vill fortsätta abonnera på HBO Nordic måste man aktivt avsluta sin prenumeration.</p>
+      <h2 class="special-font">Kostnad</h2>
+      <p>Att vara prenumerant hos HBO Nordic kostar <strong>79kr/månad</strong> och för denna summa får man tillgång till hela tjänstens utbud.</p>
+      <h2 class="special-font">Bindningstid</h2>
+      <p>HBO Nordic har ingen bindningstid och som kund kan man när som helst välja att avsluta sin prenumeration.</p>
 
+      <h1 id="streamare"class="special-font header-bigger space-above">Aktiva Streamare</h1>
+      <p>HBO Nordic tillåter upp till två aktiva skärmar samtidigt och vad det innebär i praktiken är att det går att titta på antingen två filmer eller två tv-serier samtidigt eller en kombination av dem.</p>
+      <p>Upp till 5 enheter kan vara registrerade på ett och samma konto hos HBO Nordic. I praktiken skulle det innebära att exempelvis två tv-apparater och tre telefoner i ett hushåll skulle kunna vara registrerade på ett och samma konto. Dock kan inte alla dessa enheter strömma innehåll från HBO Nordic samtidigt.</p>
+    </article>
+  </div>
     <!-- aside element -->
     <div class="article-aside right">
       <aside>
@@ -75,10 +104,9 @@
 
     <div class="article-content">
       <article>
-        <h2 id="avslutningsvis" class="special-font header-big header-space-above-normal">Avslutningsvis</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h1 id="individanpassning"class="special-font header-bigger space-above">Individanpassning</h1>
+        <p>Låt säga att du bläddrar igenom utbudet av HBO Nordics filmer och ser en hel del filmer som du tycker ser intressanta ut. För att inte glömma bort dessa filmer kan du välja att lägga till dem i &ldquo;Min Lista&rdquo;. Denna lista är det tänkt att användaren själv ska fylla med titlar som verkar intressanta och som användaren vill se på.</p>
+
       </article>
     </div>
 
