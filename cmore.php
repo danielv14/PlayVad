657a910f82b4236e50751b74fbbe5ab1c27ce675
Daniel V <daniel.vernberg@gmail.com>
2015-11-24 13:42:35 +0100

    c more är på plats

--- cmore.php
+++ cmore.php
@@ -11,22 +11,43 @@
   <div class="article-aside right">
     <aside>
       <h2 class="special-font">Innehåll</h2>
-      <h3><a class="link" href="#">Bla Bla</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#plattformar">Plattformar</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#">Kik ka</a></h3>
-      <h3 class="header-space-above-small"><a class="link" href="#avslutningsvis">Avslutningsvis</a></h3>
+      <h3><a class="link" href="#inledning">Inledning</a></h3>
+      <h3><a class="link" href="#utbud">Utbud</a></h3>
+      <h3><a class="link" href="#plattformar">Plattformar</a></h3>
+      <h3><a class="link" href="#kostnad">Kostnad och bindningstid</a></h3>
+      <h3><a class="link" href="#streamare">Aktiva streamare</a></h3>
+      <h3><a class="link" href="#individanpassning">Individanpassning</a></h3>
     </aside>
   </div>
 
   <!-- article-content non-fullscreen -->
   <div class="article-content space-below">
     <article>
-      <h1 class="special-font header-biggest">C More</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <h2 class="special-font header-big header-space-above-normal">Bla Bla</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1 id="inledning" class="special-font header-bigger">Inledning</h1>
+      <p>C More är i grunden en vanlig TV-tjänst men som anpassat sig efter den digitala frammarschen och ebjuder även sitt utbud via streaming. Utbudet består av film, tv-serier, sport och barnprogram. Tjänsten är tillgänglig på olika plattformar såsom PC, Mac, Android-enheter och iPhone. En enhet kan streama åt gången. Tjänsten är bindningsfri och innehåller tre olika paket i olika prisklasser.</p>
+
+      <h1  id="utbud"class="special-font header-bigger space-above">Utbud</h1>
+      <h2 class="special-font">Film</h2>
+      <p>Över 1500 filmer kan C More skryta med att de har i sitt utbud. De senaste filmerna från Hollywood, de mest älskade svenska biosucéerna och slutligen barnens favoriter från Astrid Lindgren är vad som utgör C Mores filmutbud.</p>
+      <p>Med genres såsom action, thriller, komedi, svenskt och science fiction finns det förmodligen något som falla alla i smaken.</p>
+      <h2 class="special-font">Tv-Serier</h2>
+      <p>Utbudet av tv-serier är tyvärr inte speciellt stort och i skrivande stund finns det 53 tv-serier att välja bland. Det C More dock kan skryta med är att många tv-serieavsnitt blir tillgängliga 24 timmar efter att de har sänts i USA.</p>
+      <p>Med bland annat kriminalserier, dramaserier och komediserier bör det finnas något för alla även om utbudet inte är speciellt stort. C Mores styrka på tv-seriefronten är helt klart att de urbjuder de senaste avsnittena så pass snabbt efter att de sänts i USA.</p>
+      <h2 class="special-font">Barn</h2>
+      <p>C More har en separat kategori med lämpligt innehåll för barn. Denna kategori är fylld med både filmer och tv-serier som lämpar sig för barn i olika åldrar. Klassiker såsom Bamse, Alfons Åberg, Pettson och Findus och slutligen klassiska verk från Astrid Lindgren ingår i <br> C Mores barnutbud.</p>
+      <h2 class="special-font">Sport</h2>
+      <p>Om man abonnerar på antingen <strong>C More Sport</strong> eller <strong>C More Max</strong> har man tillgång till <br>C Mores sportutbud som består av:</p>
+      <ul>
+      <li>Hockey: Alla matcher i SHL, HockeyAllsvenskan och SM-slutspelet.</li>
+      <li>Fotboll: Alla matcher i Allsvenskan, Superettan utvalda matcher från Ligue 1 där bland annat samtliga PSG-matcher ingår.</li>
+      <li>Tennis: De flesta och största turneringarna från ATP- och WTA-touren, grand slam-turneringen Wimbledon och matcher från Davis Cup.</li>
+      <li>Motorsport: Alla matcher från Elitserien i Speedway och Davis Cup.</li>
+      <li>Kampsport: Alla livegalor från UFC plus extramaterial.</li>
+      <li>Ridsport: FEI World Cup i dressyr.</li>
+      <li>PGA-Touren i golf (start januari 2016).</li>
+      </ul>
+      <h2 class="special-font">Dokumentärer</h2>
+      <p>C Mores utbud när det gäller dokumentärer är något litet och i skrivande stund finns endast 35 dokumentärer. Dokumentärerna handlar om allt från köttindustrin, showbiz, politik och brott. Även om antalet dokumentärer inte är så stort är bredden på utbudet stort.</p>
     </article>
   </div><!-- end of article-content non-fullscreen -->
 
@@ -37,16 +58,11 @@
       <ul class="list-col">
         <li><i class="fa fa-desktop"></i> PC</li>
         <li><i class="fa fa-apple"></i> Mac</li>
-        <li><i class="fa fa-chrome"></i> Chrome OS</li>
         <li><i class="fa fa-mobile"></i> iOS</li>
         <li><i class="fa fa-mobile"></i> Android</li>
-        <li><i class="fa fa-mobile"></i> Windows Phone</li>
         <li><i class="fa fa-chrome"></i> Chromecast</li>
-        <li><i class="fa fa-apple"></i> Apple TV</li>
-        <li><i class="fa fa-television"></i> Smart TV apparater </li>
-        <li><i class="fa fa-gamepad"></i> Playstation 3 &amp; 4</li>
-        <li><i class="fa fa-gamepad"></i> Wii &amp; Wii U</li>
-        <li><i class="fa fa-gamepad"></i> Xbox 360 &amp; Xbox One</li>
+        <li><i class="fa fa-television"></i>Lägg till i ordinarie tv-utbud</li>
+
 
 
       </ul>
@@ -55,10 +71,25 @@
 
   <div class="article-content space-above">
     <article>
-      <h2 class="special-font header-big header-space-above-normal">Kik ka</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1 id="kostnad" class="special-font header-bigger">Kostnad och bindningstid</h1>
+      <h2 class="special-font">Prova på</h2>
+      <p>C More erbjuder en prova-på period på en månad till alla potentiella nya kunder. Värt att påpeka är att sportutbudet dock <em>inte</em> ingår i denna prova-på månad.</p>
+      <p>När prova-på månaden sedan är slut kostar det <strong>99kr/månad</strong> utan att man behöver göra något. Bestämmer man sig istället för att man inte vill fortsätta vara abonnent efter att prova-på månaden är slut måste man aktivt avsluta sitt abonnemang.</p>
+      <h2 class="special-font">Kostnad</h2>
+      <p>C More erbjuder tre olika abonnemang som heter <em>C More Film</em>, <em>C More Sport</em> och slutligen<br> <em>C More Max</em>.</p>
+      <h3 class="special-font">C More Film</h3>
+      <p>Detta är grundabonnemanget och det billigaste som går att abonnera på med <strong>99kr/månad</strong>. Det som ingår i abonnemanget är filmer och tv-serier. Läs mer om utbudet <a  class="link" href="#utbud">här</a>.</p>
+      <h3 class="special-font">C More Sport</h3>
+      <p>Detta är ett abonnemang som kostar <strong>299kr/månad</strong> och är ett abonnemang som riktar sig till de som vill se på sport av alla möjliga slag. Se mer <a  class="link" href="#utbud">här</a> om sportutbudet.</p>
+      <h3 class="special-font">C More Max</h3>
+      <p>Detta abonnemang, som i grund och botten är en sammanslagning av de andra två abonnemangen kostar <strong>349kr/månad</strong> och innebär att man som kund har tillgång till alla sportsändningar, alla filmer och alla tv-serier som ingår i C Mores utbud.</p>
+      <p>Om man vet med sig att man vill ha tillgång till sport utöver det &ldquo;vanliga&rdquo; utbudet av filer och tv-serier är C More Max ett prisvärt alternativ då man tjänar <strong>50kr/månad</strong> på att abonnera på det abonnemanget istället för att abonnera på de andra två abonnemangen samtidigt.</p>
+      <h2 class="special-font">Bindningstid</h2>
+      <p>Alla abonnemang hos C More är bindningsfria och detta innebär att abonnenter när som helst kan avsluta sitt abonnemang.</p>
+
+      <h1 id="streamare"class="special-font header-bigger space-above">Aktiva Streamare</h1>
+      <p>Utbudet från C More går att streama till en skärm åt gången. Detta innebär exempelvis att det endast går att titta på en film, ett tv-serieavsnitt eller en sportsändning samtidigt.</p>
+    </article>
     </div>
 
     <!-- aside element -->
@@ -73,10 +104,9 @@
 
     <div class="article-content">
       <article>
-        <h2 id="avslutningsvis" class="special-font header-big header-space-above-normal">Avslutningsvis</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <h1 id="individanpassning" class="special-font header-bigger space-above">Individanpassning</h1>
+        <p>I skrivande stund har inte C More någon form av individanpassningen men då de inte går att strömma till mer än en enhet samtidigt verkar det inte heller som att C More riktar in sig på att vara den typen av tjänst där exempelvis olika familjemedlemmar kan nyttja samma konto men får olika upplevelser i och med individanpassning.</p>
       </article>
     </div>
 
