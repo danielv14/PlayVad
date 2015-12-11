59ccfd9b2c7765ed6b9dfbc8e0327d44fceca81b
Daniel V <daniel.vernberg@gmail.com>
2015-12-11 14:43:20 +0100

    validerad 100%

--- about.php
+++ about.php
@@ -1,9 +1,7 @@
-<!-- Set title -->
 <?php
 $title = "Om";
 ?>
 
-<!-- Include header -->
 <?php include 'header.php';?>
 
 <!-- Image full width -->
@@ -15,12 +13,12 @@ $title = "Om";
 <!-- Wrapper -->
 <div class="wrapper nonfullscreen-div space-above space-below">
   <article>
-    <h1 class="special-font header-bigger">Varför PlayVad</h1>
+    <h2 class="special-font header-bigger">Varför PlayVad</h2>
     <p>Idag finns det en uppsjö olika sätt att konsumera media på. Detta tycker vi såklart är väldigt bra. Men som med allt annat kan det bli svårt när för mycket valmöjligheter finns till vårt förfogande. Snabbare internet och bättre internettjänster har gjort att ett nytt sätt att konsumera film och tv-serier har uppstått. Människor kallar sig <a class="link" href="https://en.wikipedia.org/wiki/Cord-cutting">Cord-cutters</a> när de lämnar traditionellt tv-tittande bakom sig och vänder sig till internet och streamingtjänster.</p>
     <p>Som resultat av detta har det uppstått många så kallade streamingtjänster där kunderna själva bestämmer när, var och hur exempelvis senaste säsongen av Breaking Bad ska konsumeras. Med hjälp av streamingtjänster tar konsumenten kontroll över tv-tablån och behöver inte anpassa sig på samma sätt längre som med mer traditionellt tv-tittande. Inte längre behöver man damma av stegen, gå upp på taket och justera parabolen för att mottagningen är för dålig. Det enda som behövs är en stabil internetuppkoppling, som de flesta hushåll redan har idag, samt en prenumeration på en streamingtjänst. Men då det finns så många streamingtjänster kan det vara svårt att veta vilken som skulle passa en bäst&hellip;</p>
     <p>PlayVad finns för att råda bot på just detta problem! PlayVad finns här för att ge dig mer kunskap om vilka de ledande streamingtjänsterna är, vad de innebär och vad du kan förvänta dig av en prenumeration hos dessa. PlayVad reder ut streamingdjungeln, helt enkelt!</p>
 
-    <h1 class="special-font header-bigger space-above">Vad PlayVad gör</h1>
+    <h2 class="special-font header-bigger space-above">Vad PlayVad gör</h2>
     <p>Hur reder man då ut denna streamingdjungel kanske du undrar? Många timmars research och många liter té har gått åt till att samla in fakta om de ledande streamingtjänsterna idag. Sedan presenterar vi denna fakta för dig i hopp om att du själv ska kunna avgöra vilken eller vilka streamingtjänster som passar just <em>dig</em> bäst.</p>
     <p>Den fakta vi har valt att presentera om streamingtjänsterna är: </p>
     <ul>
