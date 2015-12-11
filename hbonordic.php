b9706e619517300b43f315a3bffebb102afa7e24
Daniel V <daniel.vernberg@gmail.com>
2015-12-11 15:22:57 +0100

    hbo nordic validerar 100%

--- hbonordic.php
+++ hbonordic.php
@@ -1,9 +1,7 @@
-<!-- Set title -->
 <?php
 $title = "HBO Nordic";
 ?>
 
-<!-- Include header -->
 <?php include 'header.php';?>
 
 <!-- Full width img netflix -->
@@ -16,8 +14,8 @@ $title = "HBO Nordic";
 
   <!-- aside element -->
   <section class="article-aside right">
+    <h2 class="special-font">Innehåll</h2>
     <aside>
-      <h2 class="special-font">Innehåll</h2>
       <ul>
         <li><a class="link" href="#inledning">Inledning</a></li>
         <li><a class="link" href="#utbud">Utbud</a></li>
@@ -31,17 +29,16 @@ $title = "HBO Nordic";
 
   <!-- article-content non-fullscreen -->
   <section class="article-content space-below">
-    <article>
-      <h1 id="inledning"class="special-font header-bigger">Inledning</h1>
+      <h2 id="inledning" class="special-font header-bigger">Inledning</h2>
       <p>HBO Nordic är det amerikanska TV-bolaget <a class="link" href="http://www.hbo.com">HBOs</a> sätt att nå ut till den nordiska marknaden. Tjänsten landerades vintern 2012 och sedan dess har det gått att streama HBO's egenproducerade tv-serier och tv-serier från andra producenter. Även om tjänsten erbjuder filmer är de flesta prenumeranter intresserade av HBO's egna tv-serier som ofta håller väldigt hög kvalité och där många anses som "klassiker". Tjänsten finns tillgänglig på många olika plattformar. Två personer kan streama samtidigt på ett och samma konto.</p>
 
-      <h1 id="utbud"class="special-font header-bigger space-above">Utbud</h1>
-      <h2 class="special-font">Film</h2>
+      <h2 id="utbud" class="special-font header-bigger space-above">Utbud</h2>
+      <h3 class="special-font">Film</h3>
       <p>I HBO Nordics utbud hittar man hundratals &ldquo;handplockade&rdquo; filmer inom genres som komedi, romantik, thriller och western. Även om utbudet av filmer inte är speciellt stort, jämfört med tv-serieutbudet finns det förmodligen något för alla inom filmutbudet.</p>
-      <h2 class="special-font">Tv-Serier</h2>
+      <h3 class="special-font">Tv-Serier</h3>
       <p>Utan att överdiva kan man säga att tv-serier är HBO Nordics största styrka och flaggskepp. Med över 3500 serieavsnitt från de allra mest hyllade tv-serierna levererar HBO Nordic tv-serier i hög klass. Tv-serier som handlar om pirater, krig och hobby-detektiver får alla plats i HBO Nordics utbud.</p>
       <p>Utbudet av tv-serier i sig är värt nog att bli prenumerant för, om man vet med sig att man gillar tv-serier från HBO.</p>
-      <h3 class="special-font">Originalserier från HBO</h3>
+      <h4 class="special-font">Originalserier från HBO</h4>
       <p>HBO Nordics största styrka är att erbjuda deras egenproducerade tv-serier som ofta är välkända och kritikerrosade. Här nedan följer ett axplock av dessa originalserier som går att streama från HBO Nordic:</p>
       <ul>
       <li>Game of Thrones</li>
@@ -54,18 +51,16 @@ $title = "HBO Nordic";
       <li>Sex and the City</li>
       <li>True Detective</li>
       </ul>
-      <h2 class="special-font">Barn</h2>
+      <h3 class="special-font">Barn</h3>
       <p>Barnutbudet hos HBO Nordic är väldigt dåligt och snudd på obefintligt. HBO Nordic har riktat in sig mot en mer äldre målgrupp och som detta lyser utbud för barn med sin frånvaro.</p>
-      <h2 class="special-font">Sport</h2>
+      <h3 class="special-font">Sport</h3>
       <p>HBO Nordic erbjuder för tillfället inga sportsändningar utan fokuserar sitt fokus till 100% på filmer och tv-serier av hög kvalité.</p>
-      <h2 class="special-font">Dokumentärer</h2>
+      <h3 class="special-font">Dokumentärer</h3>
       <p>Över 170 dokumentärer ingår i utbudet från HBO Nordic. Dokumentärerna handlar om spridda ämnen, om allt från medicin till elefanter och kriminalporträtt.</p>
-    </article>
   </section><!-- end of article-content non-fullscreen -->
 
   <!-- article-content-fullscreen -->
   <section id="plattformar" class="article-content-fullscreen">
-    <article>
       <h2 class="special-font header-big header-space-above-normal header-center">HBO Nordic finns på följade plattformar</h2>
       <ul class="list-col">
         <li><i class="fa fa-desktop"></i> PC</li>
@@ -81,48 +76,41 @@ $title = "HBO Nordic";
 
 
       </ul>
-    </article>
   </section><!-- End of article-content-fullscreen -->
 
 
 
   <section class="article-content space-above">
-    <article>
-      <h1 id="kostnad"class="special-font header-bigger">Kostnad och bindningstid</h1>
-      <h2 class="special-font">Prova på</h2>
+      <h2 id="kostnad" class="special-font header-bigger">Kostnad och bindningstid</h2>
+      <h3 class="special-font">Prova på</h3>
       <p>HBO Nordic tillåter potentiella nya kunder en prova-på period på en månad, kostnadsfritt. Under denna prova-på månad går det att nyttja hela HBO Nordics utbud till 100%. Om man skulle vilja fortsätta prenumerationen efter prova-på månaden behövs inget göras. Abonnemanget övergår till ett vanligt abonnemang efter att det har gått en månad. Om man däremot inte vill fortsätta abonnera på HBO Nordic måste man aktivt avsluta sin prenumeration.</p>
-      <h2 class="special-font">Kostnad</h2>
+      <h3 class="special-font">Kostnad</h3>
       <p>Att vara prenumerant hos HBO Nordic kostar <strong>79kr/månad</strong> och för denna summa får man tillgång till hela tjänstens utbud.</p>
-      <h2 class="special-font">Bindningstid</h2>
+      <h3 class="special-font">Bindningstid</h3>
       <p>HBO Nordic har ingen bindningstid och som kund kan man när som helst välja att avsluta sin prenumeration.</p>
 
-      <h1 id="streamare"class="special-font header-bigger space-above">Aktiva Streamare</h1>
+      <h2 id="streamare" class="special-font header-bigger space-above">Aktiva Streamare</h2>
       <p>HBO Nordic tillåter upp till två aktiva skärmar samtidigt och vad det innebär i praktiken är att det går att titta på antingen två filmer eller två tv-serier samtidigt eller en kombination av dem.</p>
       <p>Upp till 5 enheter kan vara registrerade på ett och samma konto hos HBO Nordic. I praktiken skulle det innebära att exempelvis två tv-apparater och tre telefoner i ett hushåll skulle kunna vara registrerade på ett och samma konto. Dock kan inte alla dessa enheter strömma innehåll från HBO Nordic samtidigt.</p>
-    </article>
   </section>
 
   <!-- aside element -->
   <section class="article-aside right">
-    <aside>
       <h2 class="special-font">Läs även om...</h2>
       <ul>
         <li><a class="link" href="viaplay.php">Viaplay</a></li>
         <li><a class="link" href="netflix.php">Netflix</a></li>
         <li><a class="link" href="cmore.php">C More</a></li>
       </ul>
-    </aside>
   </section>
 
     <section class="article-content">
-      <article>
-        <h1 id="individanpassning"class="special-font header-bigger space-above">Individanpassning</h1>
+        <h2 id="individanpassning" class="special-font header-bigger space-above">Individanpassning</h2>
         <p>Låt säga att du bläddrar igenom utbudet av HBO Nordics filmer och ser en hel del filmer som du tycker ser intressanta ut. För att inte glömma bort dessa filmer kan du välja att lägga till dem i &ldquo;Min Lista&rdquo;. Denna lista är det tänkt att användaren själv ska fylla med titlar som verkar intressanta och som användaren vill se på.</p>
-        <h1 class="special-font header-bigger space-above">Har du bestämt dig?</h1>
+        <h2 class="special-font header-bigger space-above">Har du bestämt dig?</h2>
         <p>Förhoppningsvis har du nu PlayVad hjälpt dig att få en bättre uppfattning om streamingtjänsten HBO Nordic och vad som erbjuds. Vill du börja prenumerera på HBO Nordic tryck på knappen här nedan för att gå till den officiella hemsidan.</p>
         <br>
         <a class="button" href="http://www.hbonordic.se" target="_blank">HBO Nordic</a>
-      </article>
     </section>
 
 </div><!-- end of parent-fullscreen wrapper -->
