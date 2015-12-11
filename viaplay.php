d259bae14da73a1030e891a9d060dcea47dff6f6
Daniel V <daniel.vernberg@gmail.com>
2015-12-11 15:30:59 +0100

    viaplay validerar 100%

--- viaplay.php
+++ viaplay.php
@@ -1,9 +1,7 @@
-<!-- Set title -->
 <?php
 $title = "Viaplay";
 ?>
 
-<!-- Include header -->
 <?php include 'header.php';?>
 
 <!-- Full width img netflix -->
@@ -15,8 +13,7 @@ $title = "Viaplay";
 <div class="wrapper fullscreen-div space-above space-below">
 
   <!-- aside element -->
-  <section class="article-aside right">
-    <aside>
+  <aside class="article-aside right">
       <h2 class="special-font">Innehåll</h2>
       <ul>
         <li><a class="link" href="#inledning">Inledning</a></li>
@@ -26,26 +23,24 @@ $title = "Viaplay";
         <li><a class="link" href="#streamare">Aktiva streamare</a></li>
         <li><a class="link" href="#individanpassning">Individanpassning</a></li>
       </ul>
-    </aside>
-  </section> <!-- end of aside element -->
+  </aside> <!-- end of aside element -->
 
   <!-- article-content non-fullscreen -->
   <section class="article-content space-below">
-    <article>
-      <h1 id="inledning" class="special-font header-bigger">Inledning</h1>
+      <h2 id="inledning" class="special-font header-bigger">Inledning</h2>
       <p>Viaplay är namnet på <a class="link" href="https://www.viasat.se">Viasats</a> onlinetjänst som erbjuder filmer, TV-serier och sport till en fast månadskostnad. Notera att kostnade ökar om en användre vill se på sport. Tjänsten lanserades redan 2007 men under ett annat namn och 2011 lanserades tjänsten under det nuvarande namnet och under ett eget bolag. Viaplay finns tillgänglig på plattformar såsom PC, Mac, Android- och iOS-enheter, smarta TV-apparater samt spelkonsoller.</p>
 
-      <h1 id="utbud" class="special-font header-bigger space-above">Utbud</h1>
+      <h2 id="utbud" class="special-font header-bigger space-above">Utbud</h2>
       <p>Viaplay sammarbetar med bland annat Warner Brothers, Walt Disney, Fox, Sony Pictures och Svensk Filmindustri för att kunna leverara bra innehåll till sina kunder.</p>
-      <h2 class="special-font">Film</h2>
+      <h3 class="special-font">Film</h3>
       <p>Viaplay erbjuder prenumeranter tusentals filmer inom en rad olika genrer såsom rysare, skräck, nodiskt och action. Kort sagt finns det förhoppningsvis något för alla när det gäller Viaplays filmutbud.</p>
       <p>Med effektiva filtrering och kategoriseringsmöjligheter är det enkelt att få sig en överblick om filmutbudet. Att Viaplay visar IMDb-betyg från hemsidan <a class="link" href="http://www.imdb.com">IMDb</a> knutet till varje film innebär att man som användare snabbt kan bilda sig en uppfattning om vad allmänheten tycker om en film. Att det dessutom går att sortera filmer efter högst betyg men även årtal, populära och senast tillagda gör att användaren har flera olika sätt att hitta nya filmer på.</p>
-      <h2 class="special-font">Tv-Serier</h2>
+      <h3 class="special-font">Tv-Serier</h3>
       <p>Amerikanska toppserier, realityprogram och svenska tv-serier är bland annat det som utgör Viaplays utbud av tv-serier. Precis som med filmerna visas alla tv-serier med tillhörande IMDb-betyg för att användare ska bilda sig en uppfattning om allmänhetens uppfattning om en tv-serie. Utbudet går att filtrera och sortera på en rad olika sätt däribland efter senast tillagda och efter högst betyg.</p>
       <p>Tv-serier inom exempelvis genrena action, kriminaldrama, komedi och reality finns att konsumera, däribland exempelvis den klassiska kultserien Seinfeld och svenska Paradise Hotel. Med sitt breda utbud finns det något för alla att titta på, oavsett smak.</p>
-      <h2 class="special-font">Barn</h2>
+      <h3 class="special-font">Barn</h3>
       <p>Viaplay är bra på att separera det ordinarie innehållet och innehållet som riktar sig till barn. Med innehåll från bland annat Disney, Nickelodeon, Cartoon Network finns det något för alla barn oavsett ålder. Barnkateogorin innehåller både filmer och tv-serier där det går att filtrera innehållet efter ett barns ålder. Om exempelvis ett barn är mellan 0-3 år går det att enkelt filtrera innehåll efter detta.</p>
-      <h2 class="special-font">Sport</h2>
+      <h3 class="special-font">Sport</h3>
       <p>Viaplay kan erbjuda sina kunder sport via abonnemanget &ldquo;Viaplay Sport&rdquo;. Utöver det ordinarie utbudet av film och tv-serier ingår sport ifrån:</p>
       <ul>
       <li>Champions League och Premier League</li>
@@ -53,15 +48,13 @@ $title = "Viaplay";
       <li>Formel 1, NHL och Golf</li>
       </ul>
       <p>Läs mer om vad abbonemanget &ldquo;Viaplay Sport&rdquo; innebär <a class="link" href="#kostnad">här</a>.</p>
-      <h2 class="special-font">Dokumentärer</h2>
+      <h3 class="special-font">Dokumentärer</h3>
       <p>Med 50+ dokumentärer som handlar om allt från golf, elefanter, pandor till flygkrascher borde det finnas dokumentärer som intresserar alla, förutsatt att man tycker om dokumentärer vill säga. Även dokumentärerna visas med IMDb-betyg och går att sortera efter land annat populäritet, betyg och utgivningsårtal.</p>
-    </article>
   </section><!-- end of article-content non-fullscreen -->
 
   <!-- article-content-fullscreen -->
   <section id="plattformar" class="article-content-fullscreen">
-    <article>
-      <h2 class="special-font header-big header-space-above-normal header-center">Viaplay finns på följade plattformar</h2>
+      <h3 class="special-font header-big header-space-above-normal header-center">Viaplay finns på följade plattformar</h3>
       <ul class="list-col">
         <li><i class="fa fa-desktop"></i> PC</li>
         <li><i class="fa fa-apple"></i> Mac</li>
@@ -74,50 +67,43 @@ $title = "Viaplay";
         <li><i class="fa fa-gamepad"></i> Playstation 3 &amp; 4</li>
         <li><i class="fa fa-gamepad"></i> Xbox 360 &amp; Xbox One</li>
       </ul>
-    </article>
   </section><!-- End of article-content-fullscreen -->
 
   <!-- article content -->
   <section class="article-content space-above">
-    <article>
-      <h1 id="kostnad" class="special-font header-bigger">Kostnad och bindningstid</h1>
-      <h2 class="special-font">Prova på</h2>
+      <h2 id="kostnad" class="special-font header-bigger">Kostnad och bindningstid</h2>
+      <h3 class="special-font">Prova på</h3>
       <p>Viaplay erbjudar alla potenitella nya kunder en månads &ldquo;prova på&rdquo; period helt gratis. När första månaden är slut behövs ingenting göras för att prenumerationen ska rulla på men om man däremot inte vill fortsätta prenumerationen efter första månaden är slut måste man aktivt stänga av sitt konto.</p>
-      <h2 class="special-font">Kostnad</h2>
+      <h3 class="special-font">Kostnad</h3>
       <p>Viaplay har två olika abonnemang som kostar antingen  <b>99kr/månad</b> eller <b>299kr/månad</b>. Beroende på om man vill att sport ska ingå i sitt abonnemang eller ej stiger priset. Det som är gemensamt för abonnemangen är &ldquo;grundutbudet&rdquo; av film och tv-serier. Är man alltså inte intresserad av att titta på sport genom Viaplay finns det ingen anledning att abonnera det dyrare alternativet då den enda skillnaden är att sportutbud tillkommer.</p>
-      <h2 class="special-font">Bindningstid</h2>
+      <h3 class="special-font">Bindningstid</h3>
       <p>Bindningstid är ett koncept inte Viaplay inte använder sig av utan en prenumerant kan när som helst under prenumerationstiden välja att avlsuta sin prenumeration. Man betalar då bara för den aktiva månaden och inget mer därefter.</p>
 
-      <h1 id="streamare" class="special-font header-bigger space-above">Aktiva Streamare</h1>
+      <h2 id="streamare" class="special-font header-bigger space-above">Aktiva Streamare</h2>
       <p>Med Viaplay går det att ha två skärmar igång samtidigt. Det innebär alltså att exempelvis en person kan titta på en film medan en annan person kan kolla på senaste avsnittet av sin favorit tv-serie samtidigt.</p>
       <p>Värt at påpeka är att Viaplay endast tillåter att upp till 4 stycken enheter är registrerade på ett konto samtidigt. Alltså är det upp till 4 stycken enheter som kan strömma max 2 olika saker (filmer, tv-serier och sport) samtidigt.</p>
-    </article>
   </section> <!-- end of article content -->
 
   <!-- aside element -->
-  <section class="article-aside right">
-    <aside>
-      <h2 class="special-font">Läs även om...</h2>
+  <aside class="article-aside right">
+      <h3 class="special-font">Läs även om...</h3>
       <ul>
         <li><a class="link" href="hbonordic.php">HBO Nordic</a></li>
         <li><a class="link" href="netflix.php">Netflix</a></li>
         <li><a class="link" href="cmore.php">C More</a></li>
       </ul>
-    </aside>
-  </section> <!-- end of aside element -->
+  </aside> <!-- end of aside element -->
 
   <!-- article content -->
   <section class="article-content">
-    <article>
-      <h1 id="individanpassning" class="special-font header-bigger space-above">Individanpassning</h1>
+      <h2 id="individanpassning" class="special-font header-bigger space-above">Individanpassning</h2>
       <p>Viaplay låter användare stjärnmarkera innehåll och således skapa en lista som användaren kommer åt på flera av sina enheter. Tanken bakom detta är att användare ska kunna spara sina favoriter och innehåll som planeras att se vid ett senare tillfället</p>
-      <h2 class="special-font">Offline</h2>
+      <h3 class="special-font">Offline</h3>
       <p>Det går att spara ned innehåll för att se offline, när internetuppkoppling antingen är dålig eller obefintlig. Denna möjlighet är endast möjlig på portalbla enheter såsom iOS- och Android-enheter. Detta är väldigt bra om man är en person som exempelvis reser mycket och inte vill belasta sitt mobilnät då det många gånger kan vara begränsat.</p>
-      <h1 class="special-font header-bigger space-above">Har du bestämt dig?</h1>
+      <h2 class="special-font header-bigger space-above">Har du bestämt dig?</h2>
       <p>Förhoppningsvis har du nu PlayVad hjälpt dig att få en bättre uppfattning om streamingtjänsten Viaplay och vad som erbjuds. Vill du börja prenumerera på Viaplay tryck på knappen här nedan för att gå till den officiella hemsidan.</p>
       <br>
       <a class="button" href="http://www.viaplay.se" target="_blank">Viaplay</a>
-    </article>
   </section> <!-- end of article content -->
 
 </div><!-- end of parent-fullscreen wrapper -->
